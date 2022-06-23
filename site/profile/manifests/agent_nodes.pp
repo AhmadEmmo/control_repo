e9b2bf9b10b22ed7915548d9c8c12cc7295d7083
AhmadEmmo <106186338+AhmadEmmo@users.noreply.github.com>
2022-06-23 05:16:15 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,6 +1,14 @@
+
+class { 'docker':
+  use_upstream_package_source => false,
+  service_overrides_template  => false,
+  docker_ce_package_name      => 'docker',
+}
+
 class profile::agent_nodes {
  include dockeragent
  dockeragent::node {'web.puppet.vm':}
  dockeragent::node {'db.puppet.vm':}
 
 }
+
